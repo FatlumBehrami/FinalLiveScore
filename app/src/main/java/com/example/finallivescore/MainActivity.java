0abab99f848562f30ecb5909fd2597b86ab6b5df
FatlumBehrami <fatlumbehrami39@gmail.com>
2021-07-21 15:40:52 +0200

    api connection

--- app/src/main/java/com/example/finallivescore/MainActivity.java
+++ app/src/main/java/com/example/finallivescore/MainActivity.java
@@ -6,13 +6,29 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.finallivescore.Basketball.BasketballFragment;
 import com.example.finallivescore.Football.FootballFragment;
 import com.example.finallivescore.Tennis.TennisFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
 
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new FootballFragment()).commit();
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url ="https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+//                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                try {
+                    JSONArray vargu = response.getJSONArray("Stages");
+                    JSONObject obj = vargu.getJSONObject(0);
+                    String id = obj.getString("Sid");
+                    Toast.makeText(MainActivity.this, id, Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Something wrong", Toast.LENGTH_SHORT).show();
+            }
+        })
+            {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("x-rapidapi-key", "62183c7184msh10a9b9ba8656549p1d00bfjsn1b0561c178bb");
+                params.put( "x-rapidapi-host", "livescore6.p.rapidapi.com");
+
+                return params;
+            }};
+        queue.add(request);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
